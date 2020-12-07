27909352e1b0cde897ad2dedaed99a31bcd1c2de
Andy Brown <asbrown002@gmail.com>
2020-12-07 10:46:43 -0800

    set up tooling

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "prettier/@typescript-eslint",
+  ],
+  plugins: ["import"],
+  env: {
+    es6: true,
+    node: true,
+  },
+  rules: {
+    "import/first": "error",
+    "import/order": ["error", { "newlines-between": "always" }],
+
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+  },
+};
